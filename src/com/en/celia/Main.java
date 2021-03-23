58247fa41c31993583049b8aea5b1ed43d1d0710
celia <celia.stir@endava.com>
2021-03-23 11:09:01 +0200

    Tema 2

--- src/com/en/celia/Main.java
+++ src/com/en/celia/Main.java
@@ -3,6 +3,6 @@ package com.en.celia;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Started main method:");
+        System.out.println("Started      main method:");
     }
 }
