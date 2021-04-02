f1692db258374499963ca30e3f133c6b868b8364
diana <diana.bardasan@endava.com>
2021-04-02 11:51:51 +0300

    delete old homework3

--- src/com/en/diana/Homework3OOP/SalaryCalculation.java
+++
@@ -1,7 +0,0 @@
-package com.en.diana.Homework3OOP;
-
-public interface SalaryCalculation {
-    public static final int PERTANGE_OF_ENCREASING = 10;
-    public double calculatePay();
-    public double increaseSalary();
-}
